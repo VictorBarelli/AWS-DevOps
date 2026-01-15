addb58b1df742baade296517798bf300c9a682c6
victor barelli <vbarelli1@gmail.com>
2026-01-15 16:24:14 -0300

    fix: add missing ECR/ECS permissions to IAM user

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,20 +2,57 @@ data "aws_iam_user" "gameswiper" {
   user_name = "GameSwiper"
 }
 
-resource "aws_iam_user_policy" "cloudfront_invalidation" {
-  name = "cloudfront-invalidation"
+resource "aws_iam_user_policy" "cd_permissions" {
+  name = "cd-pipeline-permissions"
   user = data.aws_iam_user.gameswiper.user_name
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Sid    = "CloudFrontAccess"
         Effect = "Allow"
         Action = [
           "cloudfront:CreateInvalidation",
           "cloudfront:GetDistribution",
           "cloudfront:ListDistributions"
         ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRAccess"
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ECSAccess"
+        Effect = "Allow"
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices",
+          "ecs:RegisterTaskDefinition",
+          "ecs:DescribeTaskDefinition"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "IAMPassRole"
+        Effect = "Allow"
+        Action = "iam:PassRole"
         Resource = "*"
       }
     ]

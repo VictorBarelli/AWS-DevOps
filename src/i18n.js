44c9df40c9827f46d1c134f140de018c7c44fc62
victor barelli <vbarelli1@gmail.com>
2026-01-29 11:09:26 -0300

    fix(i18n): bundle translation files to prevent loading errors

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+
+import ptTranslation from './locales/pt/translation.json';
+import enTranslation from './locales/en/translation.json';
 
 i18n
-    // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-    // learn more: https://github.com/i18next/i18next-http-backend
-    .use(Backend)
     // detect user language
     // learn more: https://github.com/i18next/i18next-browser-languagedetector
     .use(LanguageDetector)
@@ -15,16 +14,19 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
+        resources: {
+            pt: {
+                translation: ptTranslation
+            },
+            en: {
+                translation: enTranslation
+            }
+        },
         fallbackLng: 'pt',
         debug: true,
 
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
-        },
-
-        // Path configuration for loading resources
-        backend: {
-            loadPath: '/locales/{{lng}}/translation.json',
         }
     });
 
